4428da06cc2b90b55a5ce77012dfc8a8aa3806ed
Bibhuti2022 <102380026+Bibhuti2022@users.noreply.github.com>
2022-09-01 09:10:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ol>
+      <li key = "location1">Goa(India)</li>
+      <li key = "location2">Amsterdam(Netherlands)</li>
+      <li key = "location3">Delhi(India)</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
